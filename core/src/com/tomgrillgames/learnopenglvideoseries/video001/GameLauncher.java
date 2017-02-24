0528d2e73d65e9639fad6f6b1c0b572dac85a938
Thomas Pronold <tp@tpronold.de>
2017-02-24 10:19:15 +0100

    Create launcher class for video 001

---
+++ core/src/com/tomgrillgames/learnopenglvideoseries/video001/GameLauncher.java
@@ -0,0 +1,8 @@
+package com.tomgrillgames.learnopenglvideoseries.video001;
+
+public class GameLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello learn openGL!");
+    }
+}
